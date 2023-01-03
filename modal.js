368c25f0c29fd56235b0a2f0e2a7d9ccb62b8335
Maxime Zinderstein <maximezinderstein@gmail.com>
2023-01-03 14:46:11 -0500

    factorisation de la fonction ValidateDate. Modification des messages d'erreur qui viennent maintenant de chaque fonction

--- modal.js
+++ modal.js
@@ -10,7 +10,6 @@ function editNav() { // Ouverture du menu burger
 // DOM Elements
 const modalbg = document.querySelector(".bground"); //modal
 const modalBtn = document.querySelectorAll(".modal-btn"); //bouton d'inscription pour ouvrir le formulaire
-const formData = document.querySelectorAll(".formData"); // Chaque partie du formulaire
 const modalClose = document.querySelectorAll(".close"); // Croix pour fermer la modal
 const reserve = document.forms["reserve"]; // Formulaire "reserve"
 const terms = document.querySelector('.terms'); // checkbox CGU
@@ -31,40 +30,31 @@ function closeModal() {
 function validate(e) {
   e.preventDefault();
 
-  const firstName = document.getElementById('first').value.trim(); // valeur entrée dans le champ du prénom
-  const lastName = document.getElementById('last').value.trim(); // valeur entrée dans le champ du nom
-  const email = document.getElementById('email').value.trim(); // valeur entrée dans le champ de l'email
-  const birthdate = document.getElementById('birthdate').value.trim(); // valeur entrée dans le champ de la date de naissance
-  const tourneys = document.getElementById('quantity').value; // valeur entrée dans le champ du nombre de tournois
-  const checkbox2 = document.getElementById('checkbox2'); // Checkbox être prévenu des évènements
+  const firstName = document.getElementById('first'); //champ du prénom
+  const lastName = document.getElementById('last'); //champ du nom
+  const email = document.getElementById('email'); //champ de l'email
+  const birthdate = document.getElementById('birthdate'); //champ de la date de naissance
+  const tourneys = document.getElementById('quantity'); // champ du nombre de tournois
+  const checkbox2 = document.getElementById('checkbox2'); //Checkbox être prévenu des évènements
   // Liste des champs vérifiés par la fonction appropriée dans une constante:
-  const validation = [validateName(firstName), validateName(lastName), validateEmail(email), validateDate(birthdate), validateQuantity(tourneys), validateLocation(), validateCheckbox(terms)];
+  const validations = [validateName(firstName, 'prénom'), validateName(lastName, 'nom'), validateEmail(email), validateDate(birthdate), validateQuantity(tourneys), validateLocation(), validateCheckbox(terms)];
 
   // Loop
   let isValid = true;
   let returnValueIsValid = true;
-  for (i = 0; i < validation.length; i++) {
-    returnValueIsValid = validation[i];
 
-    if (returnValueIsValid) {
-      formData[i].dataset.errorVisible = false;
-    } else if (!returnValueIsValid) {
-      formData[i].dataset.errorVisible = true;
-      if (i == 0) {
-        formData[i].dataset.error = 'Vous devez entrer un prénom valide.';
-      } else if (i == 1) {
-        formData[i].dataset.error = 'Vous devez entrer un nom valide.';    
-      }
-    }
+  validations.forEach((validation) => {
+    returnValueIsValid = validation;
     isValid = isValid && returnValueIsValid; //Si une itération est false, IsValid sera false
-  }
+
+  });
 
   if (isValid) {
-    console.log('Prénom: ' + firstName);
-    console.log('Nom: ' + lastName);
-    console.log('Email: ' + email);
-    console.log('Date de naissance: ' + birthdate);
-    console.log('Nombre de participations: ' + tourneys);
+    console.log('Prénom: ' + firstName.value.trim());
+    console.log('Nom: ' + lastName.value.trim());
+    console.log('Email: ' + email.value.trim());
+    console.log('Date de naissance: ' + birthdate.value.trim());
+    console.log('Nombre de participations: ' + tourneys.value.trim());
     console.log('Ville: ' + locationChecked);
     console.log('CGU acceptées: ' + terms.checked);
     console.log('Souhaite être prévenu des prochains évènements: ' + checkbox2.checked);
@@ -79,15 +69,21 @@ function validate(e) {
 }
 
 // Fonction pour valider les noms
-function validateName(string) {
-  const regex = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/ ; //pattern
-  if (string.trim().length >= 2) { // plus de 2 caractères
-    if ((regex.test(string.trim())) && (!string.includes(",,")) && (!string.includes("..")) && (!string.includes("''")) && (!string.includes("--")) && (!string.trim().includes("  "))){
-        return true;
-    } else if ((regex.test(string.trim()) === false) || (string.includes(",,")) || (string.includes("..")) || (string.includes("''")) || (string.includes("--")) || string.trim().includes("  ")) {
+function validateName(string, option) {
+  nameValue = string.value.trim();
+  const regex = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/; //pattern
+  if (nameValue.length >= 2) { // plus de 2 caractères
+    if ((regex.test(nameValue)) && (!nameValue.includes(",,")) && (!nameValue.includes("..")) && (!nameValue.includes("''")) && (!nameValue.includes("--")) && (!nameValue.trim().includes("  "))) {
+      string.parentNode.dataset.errorVisible = false;
+      return true;
+    } else if ((regex.test(nameValue) === false) || (nameValue.includes(",,")) || (nameValue.includes("..")) || (nameValue.includes("''")) || (nameValue.includes("--")) || nameValue.trim().includes("  ")) {
+      string.parentNode.dataset.errorVisible = true;
+      string.parentNode.dataset.error = `Vous devez entrer un ${option} valide.`;
       return false;
     }
-  } else if (string.trim().length < 2) {
+  } else if (nameValue.length < 2) {
+    string.parentNode.dataset.errorVisible = true;
+    string.parentNode.dataset.error = `Veuillez entrer 2 caractères ou plus pour le champ du ${option}`;
     return false;
   }
 
@@ -96,156 +92,138 @@ function validateName(string) {
 // Fonction qui valide l'email
 
 function validateEmail(string) {
+  emailValue = string.value.trim();
   const regex = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
-  if (string.match(regex) && !string.includes(" ")) {
+  if (emailValue.match(regex) && !emailValue.includes(" ")) {
+    string.parentNode.dataset.errorVisible = false;
     return true;
-  } else if (!string.match(regex) || string.includes(" ")) {
-    formData[2].dataset.error = 'Veuillez entrer une adresse email valide.';
+  } else if (!emailValue.match(regex) || emailValue.includes(" ")) {
+    string.parentNode.dataset.errorVisible = true;
+    string.parentNode.dataset.error = 'Veuillez entrer une adresse email valide.';
     return false;
   }
 }
 
 // Fonction pour tester la date (date valide et âge limite)
 
-function validateDate(date) {
+function validateDate(dateInput) {
+  const date = dateInput.value.trim();
   const ageLimitMin = 12;
   const ageLimitMax = 100;
+  invalidDateErrorMsg = 'Vous devez entrer une date valide.';
+  ageLimitMinErrorMsg = "L'âge minimum de participation est de 12 ans.";
+  ageLimitMaxErrorMsg = "L'âge maximum de participation est de 100 ans.";
   let today = new Date();
-  let dateformatYYYYMMDD = /^((19\d{2})|(20\d{2}))-(((02)-(0[1-9]|[1-2][0-9]))|(((0(1|[3-9]))|(1[0-2]))-(0[1-9]|[1-2][0-9]|30))|((01|03|05|07|08|10|12)-(31)))$/;
+  let dateformatYYYYMMDD = /^\d{4}[\-\/\s]?((((0[13578])|(1[02]))[\-\/\s]?(([0-2][0-9])|(3[01])))|(((0[469])|(11))[\-\/\s]?(([0-2][0-9])|(30)))|(02[\-\/\s]?[0-2][0-9]))$/;
   let dateformatDDMMYYYY = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
+  let separator1 = date.split('/');
+  let separator2 = date.split('-');
+
+  if (separator1.length > 1) {
+    var pdate = date.split('/');
+  }
+  if (separator2.length > 1) {
+    var pdate = date.split('-');
+  }
+
+  let yyyy = 0;
+  let mm = 0;
+  let dd = 0;
 
   //Quand la date a le format YYYY/MM/DD (input type="date")
   if (date.match(dateformatYYYYMMDD)) {
-    let separator1 = date.split('/');
-    let separator2 = date.split('-');
 
-    if (separator1.length > 1) {
-      var pdate = date.split('/');
-    }
-    if (separator2.length > 1) {
-      var pdate = date.split('-');
-    }
     //parseInt va renvoyer un entier
-    let yyyy = parseInt(pdate[0]);
-    let mm = parseInt(pdate[1]);
-    let dd = parseInt(pdate[2]);
-    let age = today.getFullYear() - yyyy;
+    yyyy = parseInt(pdate[0]);
+    mm = parseInt(pdate[1]);
+    dd = parseInt(pdate[2]);
 
-    if ((today.getMonth() + 1 < mm) || (today.getMonth() + 1 == mm && today.getDate() < dd)) { // Calculer l'âge
-      age--;
-    }
-    //liste des jours dans chaque mois (par défaut, pas d'année bissextile)
-    let DaysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-    if (mm == 1 || mm > 2) { //tous les mois sauf février
-      if (dd > DaysInMonth[mm - 1]) { // plus de jours qu'il n'y a dans le mois
-        formData[3].dataset.error = 'Vous devez entrer une date valide.';
-        return false;
-      }
-    }
-    if (mm == 2) { // février
-      var leapYear = false;
-
-      if (!(yyyy % 4) && yyyy % 100 || !(yyyy % 400)) { //année bissextile
-        leapYear = true;
-      }
-      if (!leapYear && dd >= 29) {
-        formData[3].dataset.error = 'Vous devez entrer une date valide.';
-        return false;
-      }
-      if (leapYear && dd > 29) { // année bissextile
-        formData[3].dataset.error = 'Vous devez entrer une date valide.';
-        return false;
-      }
-    }
-
-    // Comparaison de l'âge aux âges limites 
-    if (age < ageLimitMin) {
-      formData[3].dataset.error = "L'âge minimum de participation est de 12 ans.";
-      return false;
-    } else if (age > ageLimitMax) {
-      formData[3].dataset.error = "L'âge maximum de participation est de 100 ans.";
-      return false;
-    }
-
-    //Quand la date a le format DD/MM/YYYY (input type="text")
   } else if (date.match(dateformatDDMMYYYY)) {
-    let separator1 = date.split('/');
-    let separator2 = date.split('-');
 
-    if (separator1.length > 1) {
-      var pdate = date.split('/');
-    }
-    if (separator2.length > 1) {
-      var pdate = date.split('-');
-    }
-    //parseInt va renvoyer un entier
-    let yyyy = parseInt(pdate[2]);
-    let mm = parseInt(pdate[1]);
-    let dd = parseInt(pdate[0]);
-    let age = today.getFullYear() - yyyy;
+    yyyy = parseInt(pdate[2]);
+    mm = parseInt(pdate[1]);
+    dd = parseInt(pdate[0]);
 
-    if ((today.getMonth() + 1 < mm) || (today.getMonth() + 1 == mm && today.getDate() < dd)) { // Calculer l'âge
-      age--;
-    }
-
-    //liste des jours dans chaque mois (par défaut, pas d'année bissextile)
-    let DaysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-    if (mm == 1 || mm > 2) { // Tous les mois sauf février
-      if (dd > DaysInMonth[mm - 1]) {
-        formData[3].dataset.error = 'Vous devez entrer une date valide.';
-        return false;
-      }
-    }
-    if (mm == 2) { // Février
-      var leapYear = false;
-
-      if (!(yyyy % 4) && yyyy % 100 || !(yyyy % 400)) { //année bissextile
-        leapYear = true;
-      }
-      if (!leapYear && dd >= 29) {
-        formData[3].dataset.error = 'Vous devez entrer une date valide.';
-        return false;
-      }
-      if (leapYear && dd > 29) {  //année bissextile
-        formData[3].dataset.error = 'Vous devez entrer une date valide.';
-        return false;
-      }
-    }
-    //Comparaison de l'âge avec nos âges limites
-    if (age < ageLimitMin) {
-      formData[3].dataset.error = "L'âge minimum de participation est de 12 ans.";
-      return false;
-    } else if (age > ageLimitMax) {
-      formData[3].dataset.error = "L'âge maximum de participation est de 100 ans.";
-      return false;
-    }
-
-  } else if (!date.match(dateformatYYYYMMDD) || !date.match(dateformatDDMMYYYY)) { //La date ne correspond à aucun format 
-    formData[3].dataset.error = 'Vous devez entrer une date valide.';
+  } else if ((!date.match(dateformatYYYYMMDD) || !date.match(dateformatDDMMYYYY)) && (date.length !== 0))  { //La date ne correspond à aucun format 
+    dateInput.parentNode.dataset.errorVisible = true;
+    dateInput.parentNode.dataset.error = invalidDateErrorMsg;
+    return false;
+  } else if (date.length == 0){
+    dateInput.parentNode.dataset.errorVisible = true;
+    dateInput.parentNode.dataset.error = "Vous devez entrer votre date de naissance.";
     return false;
   }
+
+  let age = today.getFullYear() - yyyy;
+
+  if ((today.getMonth() + 1 < mm) || (today.getMonth() + 1 == mm && today.getDate() < dd)) { // Calculer l'âge
+    age--;
+  }
+  //liste des jours dans chaque mois (par défaut, pas d'année bissextile)
+  let DaysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  if (mm == 1 || mm > 2) { //tous les mois sauf février
+    if (dd > DaysInMonth[mm - 1]) { // plus de jours qu'il n'y a dans le mois
+      dateInput.parentNode.dataset.errorVisible = true;
+      dateInput.parentNode.dataset.error = invalidDateErrorMsg;
+      return false;
+    }
+  }
+  if (mm == 2) { // février
+    var leapYear = false;
+
+    if (!(yyyy % 4) && yyyy % 100 || !(yyyy % 400)) { //année bissextile
+      leapYear = true;
+    }
+    if (!leapYear && dd >= 29) {
+      dateInput.parentNode.dataset.errorVisible = true;
+      dateInput.parentNode.dataset.error = invalidDateErrorMsg;
+      return false;
+    }
+    if (leapYear && dd > 29) { // année bissextile
+      dateInput.parentNode.dataset.errorVisible = true;
+      dateInput.parentNode.dataset.error = invalidDateErrorMsg;
+      return false;
+    }
+  }
+
+  // Comparaison de l'âge aux âges limites 
+  if (age < ageLimitMin) {
+    dateInput.parentNode.dataset.errorVisible = true;
+    dateInput.parentNode.dataset.error = ageLimitMinErrorMsg;
+    return false;
+  } else if (age > ageLimitMax) {
+    dateInput.parentNode.dataset.errorVisible = true;
+    dateInput.parentNode.dataset.error = ageLimitMaxErrorMsg;
+    return false;
+  }
+
+  dateInput.parentNode.dataset.errorVisible = false;
   return true;
 }
 
 // Fonction pour valider une quantité
 
-function validateQuantity(quantity) {
+function validateQuantity(quantityInput) {
+  quantity = quantityInput.value.trim();
+  quantityErrorMsg = 'Vous devez entrer une valeur entre 0 et 99.';
   const quantityMax = 99; //valeur maximum
   const quantityMin = 0; //valeur minimum
   const regex = /^[0-9]+$/; // Valeur numérique
-    if (regex.test(quantity)) {// Test de notre quantité pour confirmer que c'est une valeur numérique
-      if((quantityMin <= quantity) && (quantity <= quantityMax)) { // Test pour confirmer que la valeur est entre 0 et 99
+  if (regex.test(quantity)) {// Test de notre quantité pour confirmer que c'est une valeur numérique
+    if ((quantityMin <= quantity) && (quantity <= quantityMax)) { // Test pour confirmer que la valeur est entre 0 et 99
+      quantityInput.parentNode.dataset.errorVisible = false;
       return true;
-      } else if ((quantity > quantityMax) || (quantity < quantityMin)) {
-        formData[4].dataset.error = 'Vous devez entrer une valeur entre 0 et 99.';
-        return false;
-      }
-    } else if (!regex.test(quantity)) {
-      formData[4].dataset.error = 'Vous devez entrer une valeur entre 0 et 99.';
+    } else if ((quantity > quantityMax) || (quantity < quantityMin)) {
+      quantityInput.parentNode.dataset.errorVisible = true;
+      quantityInput.parentNode.dataset.error = quantityErrorMsg;
       return false;
     }
+  } else if (!regex.test(quantity)) {
+    quantityInput.parentNode.dataset.errorVisible = true;
+    quantityInput.parentNode.dataset.error = quantityErrorMsg;
+    return false;
+  }
 }
 
 //Fonction pour valider la localisation
@@ -254,10 +232,12 @@ function validateLocation() {
   for (i = 0; i < locations.length; i++) {
     if (locations[i].checked) {
       locationChecked = locations[i].value;
+      document.getElementById('location1').parentNode.dataset.errorVisible = false;
       return true;
     }
   }
-  formData[5].dataset.error = 'Vous devez cocher une case.';
+  document.getElementById('location1').parentNode.dataset.errorVisible = true;
+  document.getElementById('location1').parentNode.dataset.error = 'Vous devez choisir une option.';
   return false;
 }
 
@@ -265,9 +245,11 @@ function validateLocation() {
 
 function validateCheckbox(checkbox) {
   if (checkbox.checked) {
+    checkbox.parentNode.dataset.errorVisible = false;
     return true;
   } else if (!checkbox.checked) {
-    formData[6].dataset.error = "Vous devez accepter les conditions d'utilisation";
+    checkbox.parentNode.dataset.errorVisible = true;
+    checkbox.parentNode.dataset.error = "Vous devez vérifier que vous acceptez les termes et conditions.";
     return false;
   }
 }
